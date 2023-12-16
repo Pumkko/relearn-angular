0f4c0069504192648ad68381c819c67b1a39db71
Puma <c.coque@hotmail.fr>
2023-12-16 13:43:23 -0500

    ignore environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,12 @@
-export const environment = {
-    production: true,
+
+export type Environment = {
+    production: boolean;
+    auth0Domain: string;
+    auth0ClientId: string
+}
+
+export const environment: Environment = {
+    production: false,
     auth0Domain: '',
     auth0ClientId: ''
 };
