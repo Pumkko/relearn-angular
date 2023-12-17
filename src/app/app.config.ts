bc61433a55ea531f14d79097086ef88b1c0b8b15
Pumkko <charles.coque@gmail.com>
2023-12-17 13:33:42 -0500

    Feature/translate (#3)
    * try with azure webapp
    
    * setup pnpm
    
    * oupsie
    
    * undelete static web app
    
    * add what did i learn
    
    * add i18n
    
    ---------
    
    Co-authored-by: Puma <c.coque@hotmail.fr>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,30 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { QueryClient, provideAngularQuery } from '@tanstack/angular-query-experimental';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { environment } from '../environments/environment';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en'
+    })),
     provideAuth0({
       domain: environment.auth0Domain,
       clientId: environment.auth0ClientId,
--- src/app/navbar/language-picker/language-picker.component.ts
+++ src/app/navbar/language-picker/language-picker.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavbarLanguageFrenchComponent } from './french/navbar-language-french.component';
 import { NavbarLanguageUsEnglish } from './us-english/navbar-language-us-english.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-language-picker',
   standalone: true,
-  imports: [NavbarLanguageFrenchComponent, NavbarLanguageUsEnglish],
+  imports: [NavbarLanguageFrenchComponent, NavbarLanguageUsEnglish, TranslateModule],
   templateUrl: './language-picker.component.html',
 })
 export class LanguagePickerComponent {
+  translateService = inject(TranslateService);
+
+
+  translateTo(language: 'en' | 'fr') {
+    this.translateService.use(language);
+  }
+
+
+  get currentLanguage(): string {
+    return this.translateService.currentLang ?? this.translateService.defaultLang;
+  }
+
 
 }
