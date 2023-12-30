0de26d6b046dd4ff548211797f798061a36acb79
Puma <c.coque@hotmail.fr>
2023-12-29 20:42:19 -0500

    remove 'secrets' from environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@ export const environment: TEnvironment = {
     production: true,
     msalConfig: {
         auth: {
-            clientId: '{{MSAL_AUTH_CLIENTID}}',
-            authority: '{{MSAL_AUTHORITY_WITH_TENANT_ID}}'
+            clientId: 'f75ef192-e196-45cb-ae4b-9523b0b80f4d',
+            authority: 'https://login.microsoftonline.com/ddd56904-08bb-4352-88db-d7f4b5dbfde6'
         }
     },
     apiConfig: {
-        scopes: [{{API_SCOPE}}],
+        scopes: ['api://903b669b-6aca-4d49-b8ba-1deaf37002aa/Characters.Read'],
         uri: 'https://relearn-angular-app.azurewebsites.net/'
     }
 };
