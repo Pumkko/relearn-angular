bc052d731f2ff5a44c67d30cb98c9bf833edf9df
Puma <c.coque@hotmail.fr>
2024-01-01 15:19:12 -0500

    add i18n

--- src/app/rickAndMorty/components/rick-and-morty-characters-grid/rick-and-morty-characters-grid.component.ts
+++ src/app/rickAndMorty/components/rick-and-morty-characters-grid/rick-and-morty-characters-grid.component.ts
@@ -1,47 +1,72 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EnvironmentInjector, inject, runInInjectionContext } from '@angular/core';
 import { RickAndMortyService } from '../../services/rick-and-morty.service';
 import { AgGridModule } from 'ag-grid-angular';
-import { ColDef } from 'ag-grid-community'; // Column Definitions Interface
+import { ColDef, GridApi, GridOptions, GridReadyEvent } from 'ag-grid-community'; // Column Definitions Interface
 import { RickAndMortyCharacter } from '../../services/rick-and-morty-character';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-rick-and-morty-characters-grid',
   standalone: true,
-  imports: [AgGridModule],
+  imports: [AgGridModule, TranslateModule],
   templateUrl: './rick-and-morty-characters-grid.component.html',
   providers: [RickAndMortyService],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RickAndMortyCharactersGridComponent {
 
-  private readonly _rickAndMortyService = inject(RickAndMortyService);
+  private api: GridApi<RickAndMortyCharacter> | null = null;
+  private readonly _rickAndMortyService: RickAndMortyService;
+  private readonly _translateService: TranslateService;
 
+  constructor() {
+    this._rickAndMortyService = inject(RickAndMortyService);
+    this._translateService = inject(TranslateService);
+    this._translateService.onLangChange.pipe(takeUntilDestroyed()).subscribe(() => {
+      this.api?.refreshHeader();
+    });
+  }
+
+  defaultColDef: ColDef = {
+    flex: 1
+  }
 
   colDefs: ColDef<RickAndMortyCharacter>[] = [
     {
       colId: "name",
-      field: "name"
+      field: "name",
+      headerValueGetter: () => this._translateService.instant('rickAndMorty.gridHeaders.name')
     },
     {
       colId: "species",
-      field: "species"
+      field: "species",
+      headerValueGetter: () => this._translateService.instant('rickAndMorty.gridHeaders.species')
     },
     {
       colId: "origin",
-      field: "origin"
+      field: "origin",
+      headerValueGetter: () => this._translateService.instant('rickAndMorty.gridHeaders.origin')
     },
     {
       colId: "lifeStatus",
-      field: "lifeStatus"
+      field: "lifeStatus",
+      headerValueGetter: () => this._translateService.instant('rickAndMorty.gridHeaders.lifeStatus')
     },
     {
       colId: "validSince",
-      field: "validSince"
+      field: "validSince",
+      headerValueGetter: () => this._translateService.instant('rickAndMorty.gridHeaders.validSince')
     }
   ]
 
   get rickAndMortyCharactersQuery() {
     return this._rickAndMortyService.query;
+  }
+
+
+  onGridReady(event: GridReadyEvent<RickAndMortyCharacter>) {
+    this.api = event.api;
   }
 
 }
