d4f25d5aba6bded2cda045cec16fb465b6a93ff2
Puma <c.coque@hotmail.fr>
2024-01-01 17:34:14 -0500

    finish add character form

--- src/app/rickAndMorty/services/rick-and-morty-add-character.ts
+++ src/app/rickAndMorty/services/rick-and-morty-add-character.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { LifeStatus } from "./rick-and-morty-character";
 
 export const AddNewCharacterSchema = z.object({
-    name: z.string(),
+    name: z.string().min(1),
     lifeStatus: z.nativeEnum(LifeStatus),
-    origin: z.string(),
-    species: z.string()
+    origin: z.string().min(1),
+    species: z.string().min(1)
 });
 
 export type AddNewCharacter = z.infer<typeof AddNewCharacterSchema>;
