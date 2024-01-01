045a258abd1db5104116516ff90d528c464a1d3f
Puma <c.coque@hotmail.fr>
2024-01-01 16:33:24 -0500

    add form

--- src/app/rickAndMorty/components/add-new-character-button/add-new-character-button.component.ts
+++ src/app/rickAndMorty/components/add-new-character-button/add-new-character-button.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, RequiredValidator, Validators } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-add-new-character-button',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, ReactiveFormsModule],
   templateUrl: './add-new-character-button.component.html',
 })
 export class AddNewCharacterButtonComponent {
 
+  private fb = inject(FormBuilder)
+
+  addNewCharacterForm = this.fb.group({
+    name: ['', Validators.required],
+    origin: ['', Validators.required],
+    species: ['', Validators.required],
+    lifeStatus: [0, Validators.required]
+  });
 }
